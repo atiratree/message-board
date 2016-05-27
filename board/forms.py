53c8c39ed8a1d04a92b43f753bbc9f18f41d86cb
joopeti <pyry.joona@helsinki.fi>
2016-05-27 14:23:10 +0200

    added login/logout/register and some css

--- board/forms.py
+++ board/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
 class MessageForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=70)
-    content = forms.CharField(label='Content', max_length=500)
+    title = forms.CharField(label='', widget=forms.TextInput(
+		attrs={'placeholder': 'Title', 'class': 'textinput'}), max_length=70)
+    content = forms.CharField(label='', widget=forms.TextInput(
+		attrs={'placeholder': 'Message content', 'class': 'textinput'}), max_length=500)
+	
+class LoginForm(forms.Form):
+	username = forms.CharField(label='Username', max_length=64)
+	password = forms.CharField(widget=forms.PasswordInput())
+	
+class RegisterForm(forms.Form):
+	username = forms.CharField(label='Username', max_length=64)
+	password = forms.CharField(label='Password', widget=forms.PasswordInput())
+	password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput())
--- board/views.py
+++ board/views.py
@@ -1,29 +1,57 @@
 import random
 from django.contrib.auth.models import User
+from django.contrib import auth
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 
-from board.forms import MessageForm
+from board.forms import MessageForm, LoginForm, RegisterForm
 from board.models import Message
 
 def index(request):     
     form = None   
-    if request.method == 'POST':
+    if request.method == 'POST' and request.user.is_authenticated():
         reqForm = MessageForm(request.POST)
-        if reqForm.is_valid():
-            user = User(username='Generic' + str(random.randint(0,99999999)))
-            user.set_password('new password')
-            user.save()
-                       
-            msg =  Message(date= timezone.now(), title=reqForm.cleaned_data["title"], content=reqForm.cleaned_data["content"], author=user)
+        if reqForm.is_valid():                       
+            msg =  Message(date= timezone.now(), title=reqForm.cleaned_data["title"], content=reqForm.cleaned_data["content"], author=request.user)
             msg.save() 
             return redirect(reverse('board:index'))
         else:
             form = reqForm 
     
     if form is None:
-        form = MessageForm()
+       form = MessageForm()
     
-    context = {'messages': Message.objects.all(), 'form': form}
+    context = {'messages': Message.objects.order_by('-date'), 'form': form, 'auth': request.user.is_authenticated()}
     return render(request, 'board/index.html', context)
+    
+def login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        reqForm = LoginForm(request.POST)
+        if reqForm.is_valid():
+            user = auth.authenticate(username=reqForm.cleaned_data["username"], password=reqForm.cleaned_data["password"])
+            if user is not None and user.is_active:
+                auth.login(request, user)
+                return redirect('/')
+        return render(request, 'board/login.html', {'form': form, 'errorMsg': 'Bad username or password!'})
+    return render(request, 'board/login.html', {'form': form})
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+    
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        reqForm = RegisterForm(request.POST)
+        if reqForm.is_valid() and reqForm.cleaned_data["password"] == reqForm.cleaned_data["password2"]:
+            try:
+                User.objects.create_user(reqForm.cleaned_data["username"], None, reqForm.cleaned_data["password"])
+                return render(request, 'board/login.html', {'errorMsg': 'user created! pls login', 'form':LoginForm()})
+            except:
+                return render(request, 'board/register.html', {'form': form, 'errorMsg': 'Username taken!'})
+        else:
+            return render(request, 'board/register.html', {'form': form, 'errorMsg': 'Passwords didn\'t match'})
+        return render(request, 'board/register.html', {'form': form, 'errorMsg': 'Pls fill all forms and repeat the password twice'})
+    return render(request, 'board/register.html', {'form': form})
