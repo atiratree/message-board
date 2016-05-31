9fab6ae979d5492b98a68af7bfbd44b9fd38c925
joopeti <pyry.joona@helsinki.fi>
2016-05-31 19:46:07 +0200

    more beautifying
    added base.html as the main template of pages. Added css for login and
    registering pages

--- board/forms.py
+++ board/forms.py
@@ -2,15 +2,17 @@ from django import forms
 
 class MessageForm(forms.Form):
     title = forms.CharField(label='', widget=forms.TextInput(
-		attrs={'placeholder': 'Title', 'class': 'textinput'}), max_length=70)
+		attrs={'placeholder': ' Title', 'class': 'textinput'}), max_length=70)
     content = forms.CharField(label='', widget=forms.TextInput(
-		attrs={'placeholder': 'Message content', 'class': 'textinput'}), max_length=500)
+		attrs={'placeholder': ' Message content', 'class': 'textinput'}), max_length=500)
 	
 class LoginForm(forms.Form):
-	username = forms.CharField(label='Username', max_length=64)
-	password = forms.CharField(widget=forms.PasswordInput())
+	username = forms.CharField(label='Username', widget=forms.TextInput(
+		attrs={'placeholder': '', 'class': 'textinput'}), max_length=64)
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'textinput'}))
 	
 class RegisterForm(forms.Form):
-	username = forms.CharField(label='Username', max_length=64)
-	password = forms.CharField(label='Password', widget=forms.PasswordInput())
-	password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput())
+	username = forms.CharField(label='Username', widget=forms.TextInput(
+		attrs={'class': 'textinput'}), max_length=64)
+	password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'textinput'}))
+	password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'textinput'}))
